de9515e04eba9bec1745ea278d7d7364396fbbe2
Nemo <me@captnemo.in>
2017-05-05 17:35:39 +0530

    Implements lookup with tests

--- alohomora/alohomora.py
+++ alohomora/alohomora.py
@@ -3,13 +3,14 @@ import json
 from credstash import listSecrets, DEFAULT_REGION
 
 GLOBALS = {
-    'ansible_managed': "This file is managed by Ansible",
+    'alohomora_managed': "This file is managed by Alohomora",
 }
 
 
 class MockStash:
     """Credstash mock class"""
-    def listSecrets(table='credential-store'):
+
+    def listSecrets(self, table='credential-store'):
         return {
             'app_key': 'fake_app_key',
             'db_password': 'fake_db_password',
@@ -22,43 +23,32 @@ class Alohomora:
 
     def __init__(self, env, app, mock=False):
         self.env = env
+        self.app = app
         if mock:
             self.stash = MockStash()
         else:
             self.stash = Credstash()
 
         self.secrets = self.stash.listSecrets(
-            table=self.make_table_name())
+            self.make_table_name())
 
-    def make_table_name():
+    def make_table_name(self):
         return f"credstash-{self.env}-{self.app}"
 
-    def lookup(self, method, key, **args):
-        self.setup_secret_cache()
-        secrets = self.stash.listSecrets()
-        if method == 'credstash':
-            # We have a table set in the lookup,
-            # it must match the table we are using
-            if 'table' in args:
-                if args['table'] == self.make_table_name():
-                    if key in self.secrets:
-                        return self.secrets[key]
-                    else:
-                        return 'alohomora'
-                    pass
-                else:
-                    # We really don't have a fallback here
-                    return 'alohomora_untouched'
-                    pass
-        elif method == = 'alohomora':
-            # TODO: Define a stricter alohomora lookup
-            pass
+    def lookup(self, key):
+        if key in self.secrets:
+            return self.secrets[key]
+        else:
+            return 'NO_SECRET'
 
-    def render(self, file):
+    def cast(self, file):
         variables = GLOBALS
         variables['env'] = self.env
         variables['lookup'] = self.lookup
-        template = Environment().from_string(
+        # This is the template variable
+        return Environment().from_string(
             source=open(file).read(), globals=variables
-        )
-        return template.render()
+        ).render()
+
+    def render(self, file):
+        return self.cast(file)
--- test/test_alohomora.py
+++ test/test_alohomora.py
@@ -1,8 +1,17 @@
 from alohomora import Alohomora
+from configparser import ConfigParser
 
 
 class TestAlohomora:
 
     def test_lookup(self):
         spell = Alohomora('prod', 'birdie', mock=True)
-        spell.render('test/files/birdie.j2')
+        string_config = '[default]\n' + spell.cast('test/files/birdie.j2')
+
+        config = ConfigParser(allow_no_value=True)
+        config.read_string(string_config)
+
+        assert 'prod-common.db.website.vpc' == config.get('default', 'DB_HOST')
+        assert 'fake_app_key' == config.get('default', 'APP_KEY')
+        assert 'fake_db_password' == config.get('default', 'DB_PASSWORD')
+        assert 'fake_secret' == config.get('default', 'APP_SECRET')
