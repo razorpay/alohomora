b756fbe029b0ac75846538fb3522c652cdd020e6
Nemo <me@captnemo.in>
2017-08-21 18:18:08 +0530

    Adds multi-target casting (#11)
    * Python compatibility changes
    
    * Adds failing test for multi-target casting
    
    * Adds support for multi-target spell casting

--- test/test_alohomora.py
+++ test/test_alohomora.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
 from razorpay.alohomora import Alohomora
-import ConfigParser
-import StringIO
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+import io
 from io import open
 import pytest
 
@@ -9,16 +12,30 @@ import pytest
 class TestAlohomora(object):
     """Alohomora cast and other tests"""
 
-    def cast_and_read(self, spell):
-        spell.cast(file('test/files/birdie.j2'))
-
-        ini_contents = open('test/files/birdie').read()
+    def read_generated_config(self, file):
+        ini_contents = open('test/files/' + file).read()
         string_config = '[default]\n' + ini_contents
 
-        config = ConfigParser.ConfigParser(allow_no_value=True)
-        config.readfp(StringIO.StringIO(string_config))
+        config = configparser.ConfigParser(allow_no_value=True)
+        config.readfp(io.StringIO(string_config))
 
         return config
+
+    def cast_and_read(self, spell):
+        spell.cast(open('test/files/birdie.j2'))
+
+        return self.read_generated_config('birdie')
+
+    def test_multi_target_cast(self):
+        spell = Alohomora('prod', 'birdie', mock=True)
+        res = spell.cast(open('test/files/birdie.j2'),
+                         open('test/files/birdie2.j2'))
+
+        config1 = self.read_generated_config('birdie')
+        config2 = self.read_generated_config('birdie2')
+
+        assert 'fake_app_key' == config1.get('default', 'APP_KEY')
+        assert 'fake_app_key' == config2.get('default', 'APP_KEY')
 
     def test_lookup(self):
         spell = Alohomora('prod', 'birdie', mock=True)
@@ -32,7 +49,7 @@ class TestAlohomora(object):
         spell = Alohomora('prod', 'birdie', mock=True)
         with pytest.raises(Exception,
                            message='Lookup failed: app_key_non_existent'):
-            spell.cast(file('test/files/birdie_fail.j2'))
+            spell.cast(open('test/files/birdie_fail.j2'))
 
     def test_canonical(self):
         spell = Alohomora('prod', 'birdie', mock=True)
